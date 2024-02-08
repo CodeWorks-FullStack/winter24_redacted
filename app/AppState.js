4c07d5c9caa6bc8a538f78cdd477cb6245cc16b7
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-08 10:46:36 -0700

    added type for intellisense

--- app/AppState.js
+++ app/AppState.js
@@ -5,28 +5,34 @@ import { createObservableProxy } from './utils/ObservableProxy.js'
 class ObservableAppState extends EventEmitter {
 
 
+  /**
+   * @type {CaseFile[]}
+   */
   caseFiles = [
-    new CaseFile({
-      title: 'Wendigo',
-      imgUrl: 'https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/02/wendigo.png',
-      agency: 'Park Ranger'
-    }),
-    new CaseFile({
-      title: 'Unicorn',
-      agency: 'Park Ranger'
-    }),
-    new CaseFile({
-      title: 'Nessie',
-      agency: 'Coast Guard',
-      imgUrl: 'https://c.files.bbci.co.uk/4B11/production/_107971291_nessie.jpg'
-    }),
-    new CaseFile({
-      title: 'Flatwoods Monster',
-      agency: 'Men In Black',
-      imgUrl: 'https://www.wvencyclopedia.org/assets/0001/9404/flatwoodsmonster_large.jpg?1315591760'
-    }),
+    // new CaseFile({
+    //   title: 'Wendigo',
+    //   imgUrl: 'https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/02/wendigo.png',
+    //   agency: 'Park Ranger'
+    // }),
+    // new CaseFile({
+    //   title: 'Unicorn',
+    //   agency: 'Park Ranger'
+    // }),
+    // new CaseFile({
+    //   title: 'Nessie',
+    //   agency: 'Coast Guard',
+    //   imgUrl: 'https://c.files.bbci.co.uk/4B11/production/_107971291_nessie.jpg'
+    // }),
+    // new CaseFile({
+    //   title: 'Flatwoods Monster',
+    //   agency: 'Men In Black',
+    //   imgUrl: 'https://www.wvencyclopedia.org/assets/0001/9404/flatwoodsmonster_large.jpg?1315591760'
+    // }),
   ]
 
+  /**
+   * @type {CaseFile | null}
+   */
   activeCaseFile = null
 
 
