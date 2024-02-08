897ce628c0fb3d82fda205af4c72571ac92067cd
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-08 09:43:18 -0700

    added data to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,28 @@
+import { CaseFile } from './models/CaseFile.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-  /**@type {import('./models/Example.js').Example[]} */
-  examples = []
+
+  caseFiles = [
+    new CaseFile({
+      title: 'Wendigo',
+      imgUrl: 'https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/02/wendigo.png',
+      agency: 'Park Ranger'
+    }),
+    new CaseFile({
+      title: 'Unicorn',
+      agency: 'Park Ranger'
+    }),
+    new CaseFile({
+      title: 'Nessie',
+      agency: 'Coast Guard',
+      imgUrl: 'https://c.files.bbci.co.uk/4B11/production/_107971291_nessie.jpg'
+    }),
+  ]
+
+
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
--- app/models/CaseFile.js
+++ app/models/CaseFile.js
@@ -3,8 +3,8 @@ import { generateId } from "../utils/GenerateId.js"
 export class CaseFile {
   constructor (data) {
     this.id = generateId()
-    console.log('length of id', this.id.length);
     this.title = data.title
+    this.agency = data.agency
     this.body = data.body
     this.imgUrl = data.imgUrl
     this.lastAccessed = new Date()
